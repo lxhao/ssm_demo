76053be2e5858c8816d0aa91518f0de46f853ed2
XRL <XRL@XRL-PC>
2015-12-24 11:24:40 +0800

    add comment model

---
+++ src/main/java/com/bkybk/model/Content.java
@@ -0,0 +1,44 @@
+package com.bkybk.model;
+
+import com.sun.jmx.snmp.Timestamp;
+
+public class Content {
+	private Integer commentid;
+	private Integer userid;
+	private Integer articleid;
+	private String content;
+	private Timestamp cmttime;
+	public Integer getCommentid() {
+		return commentid;
+	}
+	public void setCommentid(Integer commentid) {
+		this.commentid = commentid;
+	}
+	public Integer getUserid() {
+		return userid;
+	}
+	public void setUserid(Integer userid) {
+		this.userid = userid;
+	}
+	public Integer getArticleid() {
+		return articleid;
+	}
+	public void setArticleid(Integer articleid) {
+		this.articleid = articleid;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Timestamp getCmttime() {
+		return cmttime;
+	}
+	public void setCmttime(Timestamp cmttime) {
+		this.cmttime = cmttime;
+	}
+	
+	
+
+}
